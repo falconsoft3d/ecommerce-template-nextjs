0a2742842ba9021e0c3a99e019c88e5de44cc78c
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-27 22:16:27 +0100

    sc

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import BasicLayout from "../src/layouts/BasicLayout";
+import Image from 'next/image'
 
 export default function Home() {
   return (
       <BasicLayout className="home">
             <h1>Home</h1>
+            <div className="container">
+              <div className="intro-banner-wrap">
+                  <Image src="/img/banner01.jpeg" class="img-fluid rounded"
+                  width={1300} height={500}
+                  alt="banner"/>
+              </div>
+            </div>
       </BasicLayout>
   )
 }

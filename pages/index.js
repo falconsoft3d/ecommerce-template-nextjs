d1dcc08a5a26b3e57b38dc51af6c39360cc77138
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2023-01-28 12:20:03 +0100

    sc

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import BasicLayout from "../src/layouts/BasicLayout";
 import Image from 'next/image'
+import PopularProducts from "../src/components/PopularProducts";
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
                   width={1300} height={500}
                   alt="banner"/>
               </div>
+
+              <PopularProducts/>
+                
             </div>
       </BasicLayout>
   )
---
+++ src/components/PopularProducts/PopularProducts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function PopularProducts() {
+  return (
+    <>
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <h3 class="section-title">Popular Products</h3>
+        </div>
+
+        <div class="col-md-6 text-end">        
+            <a href="./store.html" class="btn btn-outline-primary float-right">Ver Todo</a>
+        </div>
+    
+    </div>
+    </>
+  )
+}
